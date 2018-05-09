d0ccbe7c2b8c2132964afad64d8e1fc1b4d92eb3
KOALANYUNGSEP <32635964+KUCINGKEPELESET@users.noreply.github.com>
2018-05-09 09:55:37 +0700

    Fuck you

--- app/src/main/java/genius/com/mymatsurat/Matsurat/Activity1.java
+++ app/src/main/java/genius/com/mymatsurat/Matsurat/Activity1.java
@@ -6,19 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import genius.com.mymatsurat.R;
 
 public class Activity1 extends AppCompatActivity {
 
     private Button btnNext;
-    private Integer i = 3;
+    private Integer i = 1;
     private String value = Integer.toString(i);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         btnNext = (Button) findViewById(R.id.button);
         btnNext.setTextColor(Color.WHITE);
